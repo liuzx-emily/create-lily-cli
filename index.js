abd2cef22a7983e94207f41ff65bcc9ae294dbd3
liuzx_emily <liuzx_emily@qq.com>
2024-06-14 11:10:29 +0800

    fix: <npm 在 publish package 时，不会把 .gitignore 文件上传到 npm 仓库>

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import process from "process";
 import path from "path";
-import { cp, writeFile } from "fs/promises";
+import { cp, writeFile, rename, rm } from "fs/promises";
 import spawn from "cross-spawn"; // 用法同 nodejs 内置的 child_process 模块，但解决了跨平台的兼容性问题
 import { fileURLToPath } from "url";
 import { dirname } from "path";
@@ -10,10 +10,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename); // __dirname 当前文件所在目录；process.cwd() 执行命令的目录
 
 async function run() {
-  const target_path = path.join(process.cwd(), "template-project");
+  const target_path = path.join(process.cwd(), "lily-template-project");
   await generateFiles(target_path); // 根据模板生成文件
   installDeps(target_path); // 安装依赖
-  initGit(target_path); // 初始化 git
+  await initGit(target_path); // 初始化 git
 }
 
 run();
@@ -27,10 +27,12 @@ function installDeps(dirPath) {
   spawn.sync("pnpm i", { stdio: "inherit", cwd: dirPath });
 }
 
-function initGit(dirPath) {
+async function initGit(dirPath) {
   const spawnOptions = { stdio: "inherit", cwd: dirPath };
   spawn.sync("git init", spawnOptions);
-  writeFile(path.join(dirPath, ".gitignore"), "node_modules");
+  // 为了解决《npm 在 publish package 时，不会把 .gitignore 文件上传到 npm 仓库》，有两种方案：
+  await rename(path.join(dirPath, "gitignore"), path.join(dirPath, ".gitignore")); // 方案1：template中给.gitignore改名。这里再改回来
+  // await writeFile(path.join(dirPath, ".gitignore"), "node_modules"); // 方案2：直接用代码创建 .gitignore
   spawn.sync("git add .", spawnOptions);
   spawn.sync("git commit", ["-m", "init project"], spawnOptions);
 }
