ff65abe162f33fba8809016c284626bc19481abb
liuzx_emily <liuzx_emily@qq.com>
2024-10-15 09:26:56 +0800

    feat: add sass support to generated project

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,10 +1,12 @@
+<script setup>
+import { ref } from "vue";
+const message = ref("模板");
+</script>
+
 <template>
   <section>
     {{ message }}
   </section>
 </template>
 
-<script setup>
-import { ref } from "vue";
-const message = ref("模板");
-</script>
+<style lang="scss" scoped></style>
